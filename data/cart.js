d14ba9f486b2b2ce4da61c4fa164ecca57909ba3
Yassine <yassinelmqilqane@gmail.com>
2025-02-23 14:21:41 -0300

    commint fick cart

--- data/cart.js
+++ data/cart.js
@@ -1,20 +1,20 @@
 
 export let cart = JSON.parse(localStorage.getItem('cart'));
 
-if(!cart) {
-    cart = [
-        {
-            productId: '832e92dc-2c74-4bfe-a6e7-b557b2d342e0',
-            quantity: 2,
-            deliveryOptionId: '1'
-        },
-        {
-            productId: '5adaa2cf-cdd2-4703-bd28-ffd7486d487e',
-            quantity: 1,
-            deliveryOptionId: '3'
-        }
-    ]
-}
+// if(!cart) {
+//     cart = [
+//         {
+//             productId: '832e92dc-2c74-4bfe-a6e7-b557b2d342e0',
+//             quantity: 2,
+//             deliveryOptionId: '1'
+//         },
+//         {
+//             productId: '5adaa2cf-cdd2-4703-bd28-ffd7486d487e',
+//             quantity: 1,
+//             deliveryOptionId: '3'
+//         }
+//     ]
+// }
 
 function saveToStorage() {
     localStorage.setItem("cart", JSON.stringify(cart));
