84be7cda93ba148140af2b153bd0d2ba4faaf9c3
Yassine <yassinelmqilqane@gmail.com>
2025-02-23 13:37:19 -0300

    SOLVED image in github

--- scripts/seuhora.js
+++ scripts/seuhora.js
@@ -19,7 +19,7 @@ products.forEach((product) => {
                 <h4 class="item-name limit-text-to-2-lines ">${product.name}</h4>
                 <div class="product-rating-container">
                     <img class="product-rating-stars"
-                        src="../images/ratings/rating-${product.rating.stars * 10}.png">
+                        src="images/ratings/rating-${product.rating.stars * 10}.png">
                     <div class="product-rating-count link-primary">
                         ${product.rating.count}
                     </div>
