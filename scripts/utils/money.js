47bfeb1bef0b6df2cc02a6f1c3a9f4b8682add6c
Yassine <yassinelmqilqane@gmail.com>
2025-02-26 13:42:19 -0300

    Remove default tests and add money tests

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,26 +1,4 @@
 export function formatCurrency(priceCents) {
-
-    // let amount = priceCents;
-    // let formatted = amount.toLocaleString('en-Us', {
-    //     style: 'currency',
-    //     currency: 'USD',
-    //     minimumFractionDigits: 0,
-    //     maximumFractionDigits: 0
-    // });
-
-    // // return (Math.round(priceCents) / 100).toFixed(2);
-    // return formatted;
-
-    
-    // // Convert cents to dollars
-    // let dollars = priceCents / 100;
-    
-    // // Format the number with two decimal places
-    // let formattedDollars = dollars.toFixed(2);
-
-    // // Add a dollar sign and return
-    // return `$${formattedDollars}`;
-
     // Convert cents to dollars
     let dollars = priceCents / 100;
 
@@ -28,10 +6,5 @@ export function formatCurrency(priceCents) {
     let formattedDollars = dollars.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
 
     return formattedDollars;
-    
 }
 
-
-// export function formatCurrency2(priceCents) {
-//     return (Math.round(priceCents) / 100).toFixed(2);
-// }
